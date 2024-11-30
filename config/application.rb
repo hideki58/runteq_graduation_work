8e867a660d37c2d56dee72a9454758b9ae7fef56
HIDEKI WATANABE <rod8s8land@gmail.com>
2024-11-30 18:08:44 +0900

    rails g 設定変更

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,12 @@ module Myapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+  
+    # Don't generate system test files.
+    config.generators.system_tests = nil
+    config.generators do |g|
+      g.skip_routes true # ルーティングを生成しない
+      g.helper false # helperを生成しない
+      g.test_framework nil # testファイルを生成しない
   end
 end
